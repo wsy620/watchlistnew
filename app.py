af9181e9b98eaf0dd15c37f7c4a2a13b3847a314
WSY <1229536509@qq.com>
2023-11-21 20:35:47 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,23 +1,132 @@
-from flask import Flask, url_for, render_template
-
-app = Flask(__name__)
 import json
+import os
+import sys
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+import click
+
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')
+# 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+import click
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    # 新增
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+    # 删除
+    # movie = Movie.query.get(1)
+    # db.session.delete(movie)  # 使用 db.session.delete() 方法删除记录，传入模型实例
+    # db.session.commit()  # 提交改动
+
+    # 修改
+    # 查询主键是 2 的 数据。直接修改对象，最后 commit 提交
+    # movie = Movie.query.get(2)
+    # movie.title = 'WALL-E'  # 直接对实例属性赋予新的值即可
+    # movie.year = '2008'
+    # db.session.commit()
+
+    # 查询
+    Movie.query.all()
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+    @classmethod
+    def to_json(cls, movie_list):
+        movie_array = []
+        for movie in movie_list:
+            movie_dict = {
+                'id': movie.id,
+                'title': movie.title,
+                'year': movie.year
+            }
+            movie_array.append(movie_dict)
+        return json.dumps(movie_array)
 
 
 @app.route('/')
-def hello():
-    return render_template("index.html", name=name, movies=movies)
+def index():
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+
+@app.route('/query')
+def query():
+    movies = Movie.query.all()  # 读取所有电影记录
+    print(movies)
+    res = []
+    res = Movie.to_json(movies)
+    # for movie in movies:
+    #     res.append(movie.to_json())
+    print(res)
+    return res
