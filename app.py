ca8953c776578abe7795f8d1cac7953e368bdc94
WSY <1229536509@qq.com>
2023-11-21 21:54:44 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import os
 import sys
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 WIN = sys.platform.startswith('win')
@@ -12,6 +12,11 @@ else:  # 否则使用四个斜线
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+
+# 需要设置 SECRET_KEY。不然会报错
+app.config['SESSION_TYPE'] = "filesystem"
+app.config['SECRET_KEY'] = os.urandom(24)
+
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
@@ -30,6 +35,31 @@ def initdb(drop):
 
 
 import click
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+    @classmethod
+    def to_json(cls, movie_list):
+        movie_array = []
+        for movie in movie_list:
+            movie_dict = {
+                'id': movie.id,
+                'title': movie.title,
+                'year': movie.year
+            }
+            movie_array.append(movie_dict)
+        return json.dumps(movie_array)
 
 
 @app.cli.command()
@@ -73,32 +103,7 @@ def forge():
     # db.session.commit()
 
     # 查询
-    Movie.query.all()
-
-
-class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
-
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    name = db.Column(db.String(20))  # 名字
-
-
-class Movie(db.Model):  # 表名将会是 movie
-
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    title = db.Column(db.String(60))  # 电影标题
-    year = db.Column(db.String(4))  # 电影年份
-
-    @classmethod
-    def to_json(cls, movie_list):
-        movie_array = []
-        for movie in movie_list:
-            movie_dict = {
-                'id': movie.id,
-                'title': movie.title,
-                'year': movie.year
-            }
-            movie_array.append(movie_dict)
-        return json.dumps(movie_array)
+    # Movie.query.all()
 
 
 @app.context_processor
@@ -107,12 +112,64 @@ def inject_user():
     return dict(user=user)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+#
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
 
 
-@app.route('/')
+# @app.route('/')
+# def index():
+#     movies = Movie.query.all()
+#     return render_template('index.html', movies=movies)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        flash('Item created.')  # 显示成功创建的提示
+        return redirect(url_for('index'))  # 重定向回主页
+
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
